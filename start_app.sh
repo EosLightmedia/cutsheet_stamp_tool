b0b775f277bf5d8264207d49570fc73317308f01
SD Graves <34170634+sdsog@users.noreply.github.com>
2024-01-23 21:40:13 -0800

    updates start_app.sh

--- start_app.sh
+++ start_app.sh
@@ -7,4 +7,4 @@ source /home/steveg/projects/cutsheet_stamp_tool/venv/bin/activate
 cd /home/steveg/projects/cutsheet_stamp_tool
 
 # Start Gunicorn with your Flask app
-/home/steveg/projects/cutsheet_stamp_tool/venv/bin/gunicorn --workers 3 --timeout 240 --bind unix:cutsheet_stamp_tool.sock app:app
+/home/steveg/projects/cutsheet_stamp_tool/venv/bin/gunicorn --workers 3 --timeout 240 --bind unix:/home/steveg/projects/cutsheet_stamp_tool/cutsheet_stamp_tool.sock app:app
