dcea4e64a88426e444d432d75ce558b95863b032
SD Graves <34170634+sdsog@users.noreply.github.com>
2024-07-10 11:18:34 -0700

    update brand_stamp.py

--- brand_stamp.py
+++ brand_stamp.py
@@ -17,56 +17,50 @@ class BrandStamp:
     def __init__(self, white_pdf):
         self.white_pdf = white_pdf
 
-def lay_branding(self, prepared_by: int):
-    chosen_style = self.BRAND_STYLE[prepared_by]
-    watermark_top_path = os.path.join('stamp-assets', f'{chosen_style}-letterhead-top.png')
-    watermark_bottom_path = os.path.join('stamp-assets', f'{chosen_style}-letterhead-bottom.png')
-    img_top = ImageReader(watermark_top_path)
-    img_bottom = ImageReader(watermark_bottom_path)
-    width_top, height_top = img_top.getSize()
-    width_bottom, height_bottom = img_bottom.getSize()
-    pdf = PdfWriter()
-    white_pdf_file = BytesIO(self.white_pdf)
-    pdfreader_obj = PdfReader(white_pdf_file)
-    total_pages = len(pdfreader_obj.pages)
+    def lay_branding(self, prepared_by: int):
+        chosen_style = self.BRAND_STYLE[prepared_by]
+        watermark_top_path = os.path.join('stamp-assets', f'{chosen_style}-letterhead-top.png')
+        watermark_bottom_path = os.path.join('stamp-assets', f'{chosen_style}-letterhead-bottom.png')
+        img_top = ImageReader(watermark_top_path)
+        img_bottom = ImageReader(watermark_bottom_path)
+        width_top, height_top = img_top.getSize()
+        width_bottom, height_bottom = img_bottom.getSize()
+        pdf = PdfWriter()
+        white_pdf_file = BytesIO(self.white_pdf)
+        pdfreader_obj = PdfReader(white_pdf_file)
+        total_pages = len(pdfreader_obj.pages)
 
-    for page_number, page in enumerate(pdfreader_obj.pages, start=1):
-        packet = BytesIO()
-
-        # Check if page.Parent and page.Parent.MediaBox are not None
-        if page.Parent is not None and page.Parent.MediaBox is not None:
+        for page_number, page in enumerate(pdfreader_obj.pages, start=1):
+            packet = BytesIO()
             page_width, page_height = int(page.Parent.MediaBox[2]), int(page.Parent.MediaBox[3])
-        else:
-            raise ValueError("Page Parent or MediaBox is None, cannot determine page dimensions.")
+            pdf_canvas = canvas.Canvas(packet, pagesize=(page_width, page_height))
+            scale_x_top = page_width / width_top
+            scale_y_top = height_top * scale_x_top
+            scale_x_bottom = page_width / width_bottom
+            scale_y_bottom = height_bottom * scale_x_bottom
+            x_top = x_bottom = y_bottom = 0
+            y_top = page_height - scale_y_top
+            #pdf_canvas.drawImage(watermark_top_path, x_top, y_top, width=page_width, height=scale_y_top, mask='auto')
+            pdf_canvas.drawImage(watermark_bottom_path, x_bottom, y_bottom, width=page_width, height=scale_y_bottom,
+                                 mask='auto')
+            # setColor to white and use the custom font
+            pdf_canvas.setFillColorRGB(1, 1, 1)  # white color
+            pdf_canvas.setFont("Karla-Medium", 10)  # use registered font
 
-        pdf_canvas = canvas.Canvas(packet, pagesize=(page_width, page_height))
-        scale_x_top = page_width / width_top
-        scale_y_top = height_top * scale_x_top
-        scale_x_bottom = page_width / width_bottom
-        scale_y_bottom = height_bottom * scale_x_bottom
-        x_top = x_bottom = y_bottom = 0
-        y_top = page_height - scale_y_top
-        #pdf_canvas.drawImage(watermark_top_path, x_top, y_top, width=page_width, height=scale_y_top, mask='auto')
-        pdf_canvas.drawImage(watermark_bottom_path, x_bottom, y_bottom, width=page_width, height=scale_y_bottom,
-                             mask='auto')
-        # setColor to white and use the custom font
-        pdf_canvas.setFillColorRGB(1, 1, 1)  # white color
-        pdf_canvas.setFont("Karla-Medium", 10)  # use registered font
-
-        page_number_str = f'{page_number}/{total_pages}'
-        pdf_canvas.drawString(page_width - 50, 20, page_number_str)  # number position
-        pdf_canvas.save()
-        packet.seek(0)  # move to the beginning of the StringIO buffer
-        stamp = PdfReader(packet)
-        # add watermark to the page
-        merger = PageMerge().add(page).add(stamp.pages[0])
-        merger.render()
-        pdf.addpage(merger.render())  # corrected line
-    # Return BytesIO object
-    branded_pdf = BytesIO()
-    pdf.write(branded_pdf)
-    branded_pdf.seek(0)
-    return branded_pdf.getvalue()
+            page_number_str = f'{page_number}/{total_pages}'
+            pdf_canvas.drawString(page_width - 50, 20, page_number_str)  # number position
+            pdf_canvas.save()
+            packet.seek(0)  # move to the beginning of the StringIO buffer
+            stamp = PdfReader(packet)
+            # add watermark to the page
+            merger = PageMerge().add(page).add(stamp.pages[0])
+            merger.render()
+            pdf.addpage(merger.render())  # corrected line
+        # Return BytesIO object
+        branded_pdf = BytesIO()
+        pdf.write(branded_pdf)
+        branded_pdf.seek(0)
+        return branded_pdf.getvalue()
 
 
 if __name__ == '__main__':
